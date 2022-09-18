087eebac25ad3aea9167d92b0e39fec795715813
Bharti277 <bhartideependra277@gmail.com>
2022-09-18 16:15:06 +0530

    Updates

---
+++ precache-manifest.68e5b50c37701a7a06e194b9aeb4bd6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80110dbb7c9d5e5e74cb",
+    "url": "/react-practice/static/css/main.90546b02.chunk.css"
+  },
+  {
+    "revision": "80110dbb7c9d5e5e74cb",
+    "url": "/react-practice/static/js/main.80110dbb.chunk.js"
+  },
+  {
+    "revision": "395ee22a4cbef4d78eec",
+    "url": "/react-practice/static/js/1.395ee22a.chunk.js"
+  },
+  {
+    "revision": "f878aa0a124d9b346b05",
+    "url": "/react-practice/static/js/runtime~main.f878aa0a.js"
+  },
+  {
+    "revision": "1c3762e9b3b731ce133cb222ba28b119",
+    "url": "/react-practice/index.html"
+  }
+];
