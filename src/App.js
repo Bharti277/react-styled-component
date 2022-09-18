143ef29b0b82d7b048896d57ed0038b6a1584a1f
Bharti277 <bhartideependra277@gmail.com>
2022-09-18 16:38:58 +0530

    change content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-import SidebarComp from "./SidebarComp";
 
 export default function App() {
   return (
     <div className="App">
-      <SidebarComp />
+      Hello there
     </div>
   );
 }
