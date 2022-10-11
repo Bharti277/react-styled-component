a63d8c17924d49faec2af31f795aa7e7e9dc673a
Bharti277 <bhartideependra277@gmail.com>
2022-10-11 18:21:27 +0530

    created json-server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
 
-  
+  useEffect(() => {
+    fetch("http://localhost:4040/students").then((result) => {
+      result.json().then((res) => setData(res));
+    });
+  }, []);
+
+
+  const saveHandler = () => {
+    console.log("I am clicked");
+    let post_data = {name, email, mobile}
+    fetch("http://localhost:4040/api/courses", {
+      method:"POST",
+      headers:{
+        "Accept":"application/json",
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(post_data)
+    }).then((result) => {
+      result.json().then((res) => setData(res))
+    })
+  }
+
   return (
     <div className="App">
-      <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-        <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <nav className="flex flex-wrap items-center justify-between p-6 bg-teal-500">
+        <div className="flex items-center flex-shrink-0 mr-6 text-white">
           <svg
-            class="fill-current h-8 w-8 mr-2"
+            className="w-8 h-8 mr-2 fill-current"
             width="54"
             height="54"
             viewBox="0 0 54 54"
@@ -16,12 +43,14 @@ function App() {
           >
             <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
           </svg>
-          <span class="font-semibold text-xl tracking-tight">NodeJs Project</span>
+          <span className="text-xl font-semibold tracking-tight">
+            NodeJs Project
+          </span>
         </div>
-        <div class="block lg:hidden">
-          <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 text-teal-200 border border-teal-400 rounded hover:text-white hover:border-white">
             <svg
-              class="fill-current h-3 w-3"
+              className="w-3 h-3 fill-current"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -30,23 +59,23 @@ function App() {
             </svg>
           </button>
         </div>
-        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div class="text-sm lg:flex-grow">
+        <div className="flex-grow block w-full lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
             <a
               href="#responsive-header"
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+              className="block mt-4 mr-4 text-teal-200 lg:inline-block lg:mt-0 hover:text-white"
             >
               Docs
             </a>
             <a
               href="#responsive-header"
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+              className="block mt-4 mr-4 text-teal-200 lg:inline-block lg:mt-0 hover:text-white"
             >
               Examples
             </a>
             <a
               href="#responsive-header"
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+              className="block mt-4 text-teal-200 lg:inline-block lg:mt-0 hover:text-white"
             >
               Blog
             </a>
@@ -54,7 +83,7 @@ function App() {
           <div>
             <a
               href="#"
-              class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+              className="inline-block px-4 py-2 mt-4 text-sm leading-none text-white border border-white rounded hover:border-transparent hover:text-teal-500 hover:bg-white lg:mt-0"
             >
               Download
             </a>
@@ -62,6 +91,34 @@ function App() {
         </div>
       </nav>
       <h2 className="text-4xl">Hello</h2>
+      <table border="1">
+       <thead>
+       <tr>
+          <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Mobile</th>
+        </tr>
+       </thead>
+        <tbody>
+          {data.map((val) => (
+            <tr>
+              <td>{val.id}</td>
+              <td>{val.name}</td>
+              <td>{val.email}</td>
+              <td>{val.mobile}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+
+
+
+        <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} /> <br /> <br />
+        <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} /> <br /> <br />
+        <input type="text" name="mobile" value={mobile} onChange={(e) => setMobile(e.target.value)} /> <br /> <br />
+        <button type="button" onClick={saveHandler}>save new user</button>
     </div>
   );
 }
