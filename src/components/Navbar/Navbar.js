afef977636dc4178e07f3a5ddd0da0737fd62e9d
Bharti277 <bhartideependra277@gmail.com>
2022-12-24 19:14:46 +0530

    navbar created

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
-  return (
-    <div className='bg-black'>
-        sjhdjshdjshdjs
-    </div>
-  )
+  return <div className="text-white bg-black">
+      <div className="navbar">
+        <div className="home">Home</div>
+        <div className="pages">Pages</div>
+        <div className="rooms">Rooms</div>
+        <div className="reservation">Reservation</div>
+        <div className="blog">Blog</div>
+        <div className="contact">Contact</div>
+      </div>
+  </div>;
 }
 
 export default Navbar;
