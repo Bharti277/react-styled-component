cc7f204d6ef6a2f9d7ac180d40159caf8a9e8e96
Bharti277 <bhartideependra277@gmail.com>
2022-12-24 21:21:43 +0530

    nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Navbar() {
   return <div className="text-center text-white bg-black">
-      <div className="flex items-center">
-        <div className="home">Home</div>
+      <div className="flex items-center justify-center p-6 space-x-8">
+        <div className="">Home</div>
         <div className="pages">Pages</div>
         <div className="rooms">Rooms</div>
         <div className="reservation">Reservation</div>
